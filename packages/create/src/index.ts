dd1840567bb6fec2853b71730fb00efee7d9ab52
momei <2262754355@qq.com>
2025-03-27 20:13:18 +0800

    feat: pase preset

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+import { parseDep } from "./parse";
+import { program } from "commander";
+
+
+program.name("@momei-x/create").description("@momei-x/create cli");
+program
+  .command("create-preset")
+  .description("create preset")
+  .option("-ts --typescript", "typescipt")
+  .option("-eslint --eslint", "eslint")
+  .option("-prettier --prettier", "prettier")
+  .option("-stylelint --stylelint", "stylelint")
+  .action(({ typescript, eslint, prettier, stylelint }) => {
+    if (typescript) {
+      parseDep("typescript");
+    } else if (eslint) {
+      parseDep("eslint");
+    } else if (stylelint) {
+      parseDep("stylelint");
+    } else if (prettier) {
+      parseDep("prettier");
+    }
+  });
+
+program.parse();

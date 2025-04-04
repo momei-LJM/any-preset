33173c8af338ef460690e118e208b114c400068e
Ljm <2262754355@qq.com>
2025-04-04 13:35:57 +0800

    chore: lint

--- packages/create/src/presets/eslint.config.js
+++ packages/create/src/presets/eslint.config.js
@@ -1,3 +1,11 @@
 import antfu from '@antfu/eslint-config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default antfu();
+export default antfu(
+  {
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  eslintConfigPrettier
+);

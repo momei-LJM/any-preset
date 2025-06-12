b37997e2fdff861dcf75a20e2a3f1b86f1a3be78
momei <2262754355@qq.com>
2025-06-12 21:50:53 +0800

    feat: update eslint preset

--- packages/create/src/presets/eslint.config.js
+++ packages/create/src/presets/eslint.config.js
@@ -1,11 +1,16 @@
 import antfu from '@antfu/eslint-config';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default antfu(
-  {
-    rules: {
-      'no-console': 'off',
-    },
+export default antfu({
+  vue: true,
+  rules: {
+    'no-console': 'off',
+    'vue/block-order': 'off',
+    'antfu/top-level-function': 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/no-mutating-props': 'off',
+    'ts/no-use-before-define': 'off',
   },
-  eslintConfigPrettier
-);
+  formatters: {
+    css: true,
+  },
+});
